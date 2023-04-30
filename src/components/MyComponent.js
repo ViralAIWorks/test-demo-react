53a091289bb2804b733a21a4f48b725eb5463ee6
Tris <100814183+ViralAIWorks@users.noreply.github.com>
2023-04-30 08:10:29 +0700

    new

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,22 @@ class MyComponent extends React.Component {
     age: 25,
   };
 
+  handleClick = (event) => {
+    // console.log("Clicked");
+    console.log(event);
+  };
+
+  handleOnMouseOver(event) {
+    console.log(event);
+  }
+
   //JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onClick={this.handleClick}>Click me!</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hover me!</button>
       </div>
     );
   }

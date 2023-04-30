9fe1c50b5f9cf929c76eef258f394947f18e04ef
Tris <100814183+ViralAIWorks@users.noreply.github.com>
2023-04-30 09:43:18 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 import UserInfo from "./UserInfo";
+import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
   //JSX
   render() {
+    const myInfo = ["ab", "d", "c"];
     return (
       <div>
         <UserInfo />
+        <br />
+        <br />
+        <DisplayInfo name="Hoi dan IT" age="30" />
+        <hr />
+        <DisplayInfo name={"Anna"} age={27} myInfo={myInfo} />
       </div>
     );
   }

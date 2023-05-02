a9a0fd1a15d94b9d8f2e8eb759421270cdafc239
Tris <100814183+ViralAIWorks@users.noreply.github.com>
2023-05-02 16:48:40 +0700

    rrdom

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Admin = (props) => {
+  return <div>Admin component</div>;
+};
+
+export default Admin;
---
+++ src/components/User/User.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = (props) => {
+  return <div>User is here</div>;
+};
+
+export default User;

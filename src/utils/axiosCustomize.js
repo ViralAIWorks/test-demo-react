0d3a965389fe0a59b925ff62656d3ad9f1b5f106
Tris <100814183+ViralAIWorks@users.noreply.github.com>
2023-05-10 08:43:31 +0700

    loadingBar

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -37,6 +37,7 @@ instance.interceptors.response.use(
     return response && response.data ? response.data : response;
   },
   function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     console.log("run error:", error.response);
